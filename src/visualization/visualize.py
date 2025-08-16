ec45fd3bad11170de6d5f5dc478a5f4958660804
sujal <sujalxv@gmail.com>
2025-08-16 22:59:06 +0530

    updated

--- src/visualization/visualize.py
+++ src/visualization/visualize.py
@@ -2,6 +2,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib as mpl
 from IPython.display import display
+import matplotlib.animation as animation
 
 # --------------------------------------------------------------
 # Load data
@@ -31,22 +32,30 @@ for label in df['label'].unique():
 # --------------------------------------------------------------
 # Adjust plot settings
 # --------------------------------------------------------------
-
+mpl.style.use('seaborn-v0_8-deep')
+mpl.rcParams['figure.figsize'] = (20, 5)
+mpl.rcParams['figure.dpi'] = 100
 
 # --------------------------------------------------------------
 # Compare medium vs. heavy sets
 # --------------------------------------------------------------
-
-
+category_df = df.query("label == 'squat' and participant == 'A'").reset_index(drop=True)
+fig , ax = plt.subplots()
+category_df.groupby(["category"])["acc_y"].plot()
+ax.set_ylabel("acc_y")
+ax.set_xlabel("sample")
+plt.legend()
 # --------------------------------------------------------------
-# Compare participants
-# --------------------------------------------------------------
-
 
 # --------------------------------------------------------------
 # Plot multiple axis
 # --------------------------------------------------------------
-
+participant_df = df.query("label == 'bench'" ).sort_values("participant").reset_index(drop=True)
+fig , ax = plt.subplots()
+participant_df.groupby(["participant"])["acc_y"].plot()
+ax.set_ylabel("acc_y")
+ax.set_xlabel("sample")
+plt.legend()
 
 # --------------------------------------------------------------
 # Create a loop to plot all combinations per sensor
